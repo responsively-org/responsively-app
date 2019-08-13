80745bebe9b250d7da8d0ff6f508c69a239869a9
Manoj Vivek <p.manoj.vivek@gmail.com>
2019-08-13 16:25:30 +0530

    Logo updated

--- desktop-app/app/components/LeftIconsPane/index.js
+++ desktop-app/app/components/LeftIconsPane/index.js
@@ -1,12 +1,13 @@
 // @flow
 import React from 'react';
 import Logo from '../icons/Logo';
+import cx from 'classnames';
 
 import styles from './styles.css';
 
 const LeftIconsPane = () => (
   <div className={styles.iconsContainer}>
-    <div className={styles.icon}>
+    <div className={cx(styles.icon, styles.logo)}>
       <Logo width={50} height={50} />
     </div>
   </div>
