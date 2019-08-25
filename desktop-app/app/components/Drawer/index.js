a43ffcde9247f9c583a7d9f9862a87912caf7904
Manoj Vivek <p.manoj.vivek@gmail.com>
2019-08-25 23:07:25 +0530

    Added button to close the drawer

--- desktop-app/app/components/Drawer/index.js
+++ desktop-app/app/components/Drawer/index.js
@@ -10,11 +10,39 @@ import cx from 'classnames';
 
 import styles from './styles.css';
 import {DEVICE_MANAGER} from '../../constants/DrawerContents';
+import {iconsColor} from '../../constants/colors';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    marginLeft: 'auto',
+    '&:hover': {},
+  },
+  iconHover: {
+    color: iconsColor,
+    opacity: 0.8,
+    borderRadius: '50%',
+    '&:hover': {
+      opacity: 1,
+      background: '#8c8c8c42',
+    },
+  },
+}));
 
 export function Drawer(props) {
-  console.log('props', props);
+  const classes = useStyles();
   return (
     <div className={cx(styles.drawer, {[styles.open]: props.drawer.open})}>
+      <div>
+        <IconButton
+          className={classes.root}
+          onClick={() => props.changeDrawerOpenState(false)}
+        >
+          <ChevronLeftIcon className={classes.iconHover} />
+        </IconButton>
+      </div>
       {getDrawerContent(props.drawer.content)}
     </div>
   );
