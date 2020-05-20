1de761333096d7b936d6a60e159a337555e44ae4
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-05-20 09:19:03 +0530

    Heap middleware

--- desktop-app/app/utils/filterUtils.js
+++ desktop-app/app/utils/filterUtils.js
@@ -2,11 +2,6 @@ import {FILTER_FIELDS} from '../reducers/browser';
 
 export const isDeviceEligible = (device, filterCriteria) => {
   if (Object.keys(filterCriteria[FILTER_FIELDS.OS]).length > 0) {
-    console.log(
-      'OS',
-      device.os,
-      filterCriteria[FILTER_FIELDS.OS].indexOf(device.os)
-    );
     if (filterCriteria[FILTER_FIELDS.OS].indexOf(device.os) === -1) {
       return false;
     }
