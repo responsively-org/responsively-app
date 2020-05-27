6ac976eeec1cf41553aa07839584896e686236a0
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-05-27 18:59:18 +0530

    Browser extension initial commit

---
+++ browser-extension/src/background.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/no-unassigned-import
+console.log('Responsively Extension');
---
+++ browser-extension/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const SizePlugin = require('size-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebExtWebpackPlugin = require('@ianwalter/web-ext-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	devtool: 'source-map',
+	stats: 'errors-only',
+	entry: {
+		background: './src/background',
+		options: './src/options'
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	plugins: [
+		new SizePlugin(),
+		new CopyWebpackPlugin([
+			{
+				from: '**/*',
+				context: 'public',
+				ignore: ['*.js']
+			},
+			{
+				from: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
+			}
+		]),
+		new WebExtWebpackPlugin({ sourceDir: path.join(__dirname, 'dist'), verbose: true }),
+	],
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					mangle: false,
+					compress: false,
+					output: {
+						beautify: true,
+						indent_level: 2 // eslint-disable-line camelcase
+					}
+				}
+			})
+		]
+	}
+};
