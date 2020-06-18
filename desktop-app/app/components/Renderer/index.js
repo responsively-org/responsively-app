4cb3f2ce36e5bcbbacc0e6fe0ff3c734a154b8b6
Gema <gem.anggada@gmail.com>
2020-06-18 18:21:00 +0700

    chore: Added sendFlipStatus function on Renderer as a props to WebView

--- desktop-app/app/components/Renderer/index.js
+++ desktop-app/app/components/Renderer/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import WebViewContainer from '../../containers/WebViewContainer';
 import cx from 'classnames';
 import Spinner from '../Spinner';
@@ -10,13 +10,23 @@ import {getDeviceIcon} from '../../utils/iconUtils';
 
 function Renderer(props) {
   const [loading, setLoading] = useState(true);
+  const [isFlip, setFlip] = useState(false);
+  let dimension = [props.device.width, 'x', props.device.height];
+
+  const sendFlipStatus = useCallback(
+    status => {
+      setFlip(status);
+    },
+    [isFlip]
+  );
+
   return (
     <div className={cx(styles.container, {[styles.hidden]: props.hidden})}>
       <div className={styles.titleContainer}>
         {getDeviceIcon(props.device.type)}
         <span className={cx(styles.deviceTitle)}>{props.device.name}</span>
         <div className={cx(styles.deviceSize)}>
-          {props.device.width}x{props.device.height}
+          {isFlip ? dimension.reverse().join('') : dimension.join('')}
         </div>
         <div className={cx(styles.loaderContainer)}>
           {loading && (
@@ -29,6 +39,7 @@ function Renderer(props) {
       <div className={cx(styles.deviceWrapper)}>
         <WebViewContainer
           device={props.device}
+          sendFlipStatus={sendFlipStatus}
           transmitNavigatorStatus={props.transmitNavigatorStatus}
           onLoadingStateChange={setLoading}
           devToolBottomRef={props.devToolBottomRef}
