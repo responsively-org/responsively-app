c1e3da9e9a09b97d4f73ae41109b566a704bbc6f
Suresh <suresh.squal@gmail.com>
2020-06-20 11:34:19 +0530

    test formatting

--- desktop-app/app/components/NavigationControls/index.js
+++ desktop-app/app/components/NavigationControls/index.js
@@ -1,69 +1,68 @@
 // @flow
-import React, {Component} from 'react'
-import cx from 'classnames'
-import Grid from '@material-ui/core/Grid'
-import {ipcRenderer} from 'electron'
-import HomeIcon from '../icons/Home'
-import {Icon} from 'flwww'
+import React, {Component} from 'react';
+import cx from 'classnames';
+import Grid from '@material-ui/core/Grid';
+import {ipcRenderer} from 'electron';
+import HomeIcon from '../icons/Home';
+import {Icon} from 'flwww';
 
-import styles from './styles.module.css'
-import commonStyles from '../common.styles.css'
-import {iconsColor} from '../../constants/colors'
-import {Tooltip} from '@material-ui/core'
+import styles from './styles.module.css';
+import commonStyles from '../common.styles.css';
+import {iconsColor} from '../../constants/colors';
+import {Tooltip} from '@material-ui/core';
 
 class NavigationControls extends Component {
-  componentDidMount () {
-    ipcRenderer.on('reload-url', this.props.triggerNavigationReload)
-    ipcRenderer.on('stop-loading-url', this.props.triggerStopLoading)
-    ipcRenderer.on('reload-css', this.props.reloadCSS)
+  componentDidMount() {
+    ipcRenderer.on('reload-url', this.props.triggerNavigationReload);
+    ipcRenderer.on('stop-loading-url', this.props.triggerStopLoading);
+    ipcRenderer.on('reload-css', this.props.reloadCSS);
   }
 
-  render () {
+  render() {
     const iconProps = {
       color: iconsColor,
       height: 25,
       width: 25,
-    }
-    const {backEnabled, forwardEnabled} = this.props
+    };
+    const {backEnabled, forwardEnabled} = this.props;
     const deviceLoading = (this.props.devices || []).find(
       device => device.loading
-    )
+    );
 
-    console.log('asdindjha s')
-    let refreshOrCancel
+    let refreshOrCancel;
     if (deviceLoading) {
       refreshOrCancel = (
         <Grid item className={cx(commonStyles.icons, commonStyles.enabled)}>
-          <Tooltip title='Stop loading this page'>
+          <Tooltip title="Stop loading this page">
             <div onClick={this.props.triggerStopLoading}>
-              <Icon type='closeBig' {...iconProps} className='closeIcon' />
+              <Icon type="closeBig" {...iconProps} className="closeIcon" />
             </div>
           </Tooltip>
         </Grid>
-      )
+      );
     } else {
       refreshOrCancel = (
         <Grid item className={cx(commonStyles.icons, commonStyles.enabled)}>
-          <Tooltip title='Reload'>
+          <Tooltip title="Reload">
             <div
               onClick={this.props.triggerNavigationReload}
               style={{transform: 'rotate(90deg)'}}
             >
               <Icon
-                type='rotate'
-                style='pointer-events:none'
+                type="rotate"
+                style="pointer-events:none"
                 {...iconProps}
-                className='reloadIcon'
+                className="reloadIcon"
               />
               {/*<ReloadIcon {...iconProps} height={15} width={15} padding={5} />*/}
             </div>
           </Tooltip>
         </Grid>
-      )
+      );
     }
     return (
       <div className={styles.navigationControls}>
-        <Grid container spacing={1} alignItems='center'>
+        <Grid container spacing={1} alignItems="center">
           <Grid
             item
             className={cx(commonStyles.icons, {
@@ -76,9 +75,9 @@ class NavigationControls extends Component {
                 [commonStyles.disabled]: !backEnabled,
               })}
             />
-            <Tooltip title='Back'>
+            <Tooltip title="Back">
               <div onClick={this.props.triggerNavigationBack}>
-                <Icon type='arrowLeft' size='30px' {...iconProps} />
+                <Icon type="arrowLeft" size="30px" {...iconProps} />
               </div>
             </Tooltip>
           </Grid>
@@ -94,9 +93,9 @@ class NavigationControls extends Component {
                 [commonStyles.disabled]: !forwardEnabled,
               })}
             />
-            <Tooltip title='Forward'>
+            <Tooltip title="Forward">
               <div onClick={this.props.triggerNavigationForward}>
-                <Icon type='arrowRight' size='30px' {...iconProps} />
+                <Icon type="arrowRight" size="30px" {...iconProps} />
                 {/*<ArrowRightIcon {...iconProps} />*/}
               </div>
             </Tooltip>
@@ -105,7 +104,7 @@ class NavigationControls extends Component {
           {refreshOrCancel}
 
           <Grid item className={cx(commonStyles.icons, commonStyles.enabled)}>
-            <Tooltip title='Go to Homepage'>
+            <Tooltip title="Go to Homepage">
               <div
                 className={commonStyles.flexAlignVerticalMiddle}
                 onClick={this.props.goToHomepage}
@@ -116,8 +115,8 @@ class NavigationControls extends Component {
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
-export default NavigationControls
+export default NavigationControls;
