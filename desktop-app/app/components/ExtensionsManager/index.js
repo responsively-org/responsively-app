07b1f5cd14971ba90f6c6399280a9f971f937e2a
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-07-01 12:29:20 +0530

    Added help info about finding extension id

--- desktop-app/app/components/ExtensionsManager/index.js
+++ desktop-app/app/components/ExtensionsManager/index.js
@@ -1,24 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/DeleteOutlined';
 import ExtensionsIcon from '@material-ui/icons/Extension';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import {remote, ipcRenderer} from 'electron';
 import cx from 'classnames';
 import styles from './styles.css';
 import commonStyles from '../common.styles.css';
-import { lightIconsColor } from '../../constants/colors';
+import {lightIconsColor} from '../../constants/colors';
+import helpScreenshot from './help-screenshot.png';
+import {
+  makeStyles,
+  Popper,
+  Fade,
+  Paper,
+  Typography,
+  ClickAwayListener,
+} from '@material-ui/core';
 
+const useStyles = makeStyles({
+  adornedEnd: {
+    paddingRight: 0,
+  },
+});
 
 export default function ExtensionsManager({triggerNavigationReload}) {
   const {BrowserWindow} = remote;
   const getInstalledExtensions = () => {
-    return Object.values(BrowserWindow.getDevToolsExtensions()).sort((a, b) => a.name.localeCompare(b.name));
+    return Object.values(BrowserWindow.getDevToolsExtensions()).sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
   };
 
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [helpOpen, setHelpOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [extensionId, setExtensionId] = useState('');
@@ -28,7 +49,7 @@ export default function ExtensionsManager({triggerNavigationReload}) {
     setErrorMessage('');
   }, [extensionId]);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     if (loading) {
       return;
@@ -37,7 +58,10 @@ export default function ExtensionsManager({triggerNavigationReload}) {
     setLoading(true);
     setErrorMessage('');
 
-    const id = extensionId.replace(/\/$/, '').split('/').pop();
+    const id = extensionId
+      .replace(/\/$/, '')
+      .split('/')
+      .pop();
 
     try {
       await ipcRenderer.invoke('install-extension', id);
@@ -60,50 +84,107 @@ export default function ExtensionsManager({triggerNavigationReload}) {
     setExtensions(getInstalledExtensions());
   };
 
+  const toggleHelp = event => {
+    if (helpOpen) {
+      event && event.stopPropagation();
+      return setHelpOpen(!helpOpen);
+    }
+    if (!event.currentTarget.type) {
+      return;
+    }
+    setAnchorEl(event.currentTarget);
+    setHelpOpen(!helpOpen);
+  };
+
   return (
-    <div className={cx(commonStyles.sidebarContentSection)}>
-      <div className={cx(commonStyles.sidebarContentSectionTitleBar)}>
-        <ExtensionsIcon className={styles.extensionManagerIcon} />  Manage Extensions
-      </div>
-      <div className={cx(commonStyles.sidebarContentSectionContainer)}>
+    <>
+      <Popper open={helpOpen} anchorEl={anchorEl} placement="bottom" transition>
+        {({TransitionProps}) => (
+          <ClickAwayListener onClickAway={toggleHelp}>
+            <Fade {...TransitionProps} timeout={350}>
+              <Paper>
+                <div className={styles.extensionsHelp}>
+                  <p className={cx(styles.extensionsHelpText)}>
+                    Find the extension on Chrome Web Store and copy the
+                    extension ID from the address bar(as shown below).
+                  </p>
+                  <img
+                    className={styles.extensionsHelpImg}
+                    src={helpScreenshot}
+                  />
+                </div>
+              </Paper>
+            </Fade>
+          </ClickAwayListener>
+        )}
+      </Popper>
+      <div className={cx(commonStyles.sidebarContentSection)}>
+        <div className={cx(commonStyles.sidebarContentSectionTitleBar)}>
+          <ExtensionsIcon className={styles.extensionManagerIcon} /> Manage
+          Extensions
+        </div>
+        <div className={cx(commonStyles.sidebarContentSectionContainer)}>
+          <form className={styles.extensionAdd} onSubmit={handleSubmit}>
+            <div>
+              <TextField
+                type="text"
+                color="secondary"
+                value={extensionId}
+                onChange={handleChange}
+                disabled={loading}
+                placeholder="Extension ID"
+                variant="outlined"
+                error={errorMessage !== ''}
+                InputProps={{
+                  classes: {
+                    adornedEnd: classes.adornedEnd,
+                  },
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <IconButton
+                        onClick={toggleHelp}
+                        size="small"
+                        title="Help"
+                      >
+                        <HelpOutlineIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <Button onClick={handleSubmit} disabled={extensionId.trim() === ''}>
+              {loading ? <CircularProgress size={22} /> : 'Add'}
+            </Button>
+          </form>
 
-        <form className={styles.extensionAdd} onSubmit={handleSubmit}>
-          <div>
-            <TextField
-              type="text"
-              color="secondary"
-              value={extensionId}
-              onChange={handleChange}
-              disabled={loading}
-              placeholder="Extension ID"
-              variant="outlined"
-              error={errorMessage !== ''}
+          {errorMessage && (
+            <FormHelperText error={true}>{errorMessage}</FormHelperText>
+          )}
+
+          <FormHelperText className={styles.extensionsNotice}>
+            Note: Only DevTool extensions will work properly, other general
+            browser extensions may not work as intended.
+          </FormHelperText>
+
+          <p className={styles.extensionsLabel}>
+            Installed extensions ({extensions.length})
+          </p>
+
+          {extensions.map(extension => (
+            <ExtensionItem
+              extension={extension}
+              key={extension.name}
+              onDelete={handleDelete}
             />
-          </div>
-          <Button onClick={handleSubmit} disabled={extensionId.trim() === ''}>
-            {loading ? <CircularProgress size={22}/> : 'Add'}
-          </Button>
-        </form>
-
-        {errorMessage && <FormHelperText error={true}>{errorMessage}</FormHelperText>}
-
-        <FormHelperText className={styles.extensionsNotice}>Note: Only DevTool extensions will work properly, other general browser extensions may not work as intended.</FormHelperText>
-
-        <p className={styles.extensionsLabel}>Installed extensions ({extensions.length})</p>
-
-        {extensions.map(extension => (
-          <ExtensionItem
-            extension={extension}
-            key={extension.name}
-            onDelete={handleDelete}
-          />
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-function ExtensionItem({ extension, onDelete }) {
+function ExtensionItem({extension, onDelete}) {
   const handleDelete = () => {
     onDelete(extension.name);
   };
@@ -115,7 +196,7 @@ function ExtensionItem({ extension, onDelete }) {
         <span className={styles.extensionVersion}>{extension.version}</span>
       </div>
       <IconButton onClick={handleDelete} size="small" title="Delete extension">
-        <DeleteIcon fontSize="small" htmlColor={lightIconsColor}/>
+        <DeleteIcon fontSize="small" htmlColor={lightIconsColor} />
       </IconButton>
     </div>
   );
