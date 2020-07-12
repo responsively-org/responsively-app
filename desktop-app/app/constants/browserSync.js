f67cc6c8383d576b0fa94500709330cbe3a001ba
Suresh <suresh.squal@gmail.com>
2020-07-12 22:16:20 +0530

    invalid character in bs url

--- desktop-app/app/constants/browserSync.js
+++ desktop-app/app/constants/browserSync.js
@@ -2,7 +2,4 @@ export const BROWSER_SYNC_PORT = 12719;
 export const BROWSER_SYNC_VERSION = '2.26.7';
 
 export const BROWSER_SYNC_HOST = `localhost:${BROWSER_SYNC_PORT}`;
-export const BROWSER_SYNC_EMBED_SCRIPT = `https://
-  ${BROWSER_SYNC_HOST}
-  /browser-sync/browser-sync-client.js?v=
-  ${BROWSER_SYNC_VERSION}`;
+export const BROWSER_SYNC_EMBED_SCRIPT = `https://${BROWSER_SYNC_HOST}/browser-sync/browser-sync-client.js?v=${BROWSER_SYNC_VERSION}`;
