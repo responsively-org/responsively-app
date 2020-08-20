6fd99970e9604fa7a89ca6e5abaf760def48d8a5
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-08-20 18:54:30 +0530

    Contributor section missing js fix

---
+++ website/js/custom/contributors.js
@@ -0,0 +1,37 @@
+const populateContributors = () => {
+  function profileHtmlString(to, title, style) {
+    return `<a href="${to}" title="${title}" target="_blank" class="github-contributors__avatar" style="${style}">&nbsp;</a>`;
+  }
+
+  function generateProfile(c) {
+    const to = `https://github.com/${c.login}`;
+    const title = `${c.contributions} contributions from ${c.login}`;
+    const style = `background-image: url('${c.avatar_url}&s=96');`;
+    return profileHtmlString(to, title, style);
+  }
+
+  function generateFooterRow(json) {
+    let html = '';
+    document.getElementById(
+      'github-contributors__thanks',
+    ).innerText = `Thanks to all of our ${json.length} contributors! 🎉👏`;
+    json.forEach((contributor) => {
+      html += generateProfile(contributor);
+    });
+    document.getElementById('github-contributors__users').innerHTML = html;
+  }
+
+  function getContr() {
+    fetch(
+      'https://api.github.com/repos/responsively-org/responsively-app/contributors',
+    )
+      .then((response) => response.text())
+      .then((text) => JSON.parse(text))
+      .then((json) => generateFooterRow(json))
+      .catch(() => { });
+  }
+
+  getContr();
+};
+
+export default populateContributors;
