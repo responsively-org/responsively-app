06dc32069baa34eefca40b530e5194be161b9fd8
gorogoroumaru <zokutyou2@gmail.com>
2020-08-30 11:04:24 +0900

    remove imports

--- desktop-app/app/utils/TextAreaWithCopyButton.js
+++ desktop-app/app/utils/TextAreaWithCopyButton.js
@@ -1,11 +1,5 @@
 import React, {useState} from 'react';
-import FormControl from '@material-ui/core/FormControl';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import Tooltip from '@material-ui/core/Tooltip';
 import CopyToClipBoard from 'react-copy-to-clipboard';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
