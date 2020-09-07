06b03e6a236c4c50fa51dea972d5dda46622262c
Max Kelly <maxwill.kelly@gmail.com>
2020-09-07 17:13:10 +0100

    Converts DeviceManger to component class if necessary

--- desktop-app/app/components/DeviceManager/index.js
+++ desktop-app/app/components/DeviceManager/index.js
@@ -1,5 +1,5 @@
-import React, {useState, Fragment, useEffect, useRef} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {Component, createRef, Fragment} from 'react';
+import {withStyles} from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -16,10 +16,11 @@ import LightBulbIcon from '../icons/LightBulb';
 import DeviceList from './DeviceList';
 import AddDeviceContainer from '../../containers/AddDeviceContainer';
 import ErrorBoundary from '../ErrorBoundary';
+import Mousetrap from 'mousetrap';
 
 import styles from './styles.css';
 
-const useStyles = makeStyles(theme => ({
+const additionalStyles = theme => ({
   appBar: {
     position: 'relative',
   },
@@ -27,71 +28,86 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(2),
     flex: 1,
   },
-}));
+});
 
-export default function DeviceManager(props) {
-  const [open, setOpen] = useState(false);
-  const classes = useStyles();
-  // const inactiveDeviceList = useRef();
+class DeviceManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      devices: {
+        active: [],
+        inactive: [],
+        inactiveFiltered: [],
+      },
+    };
+    // this.inactiveDeviceList = createRef();
+  }
 
-  const [devices, setDevices] = useState({
-    active: [],
-    inactive: [],
-    inactiveFiltered: [],
-  });
-
-  useEffect(() => {
-    const activeDevices = props.browser.devices;
+  componentDidMount() {
+    const activeDevices = this.props.browser.devices;
     const activeDevicesById = activeDevices.reduce((acc, val) => {
       acc[val.id] = val;
       return acc;
     }, {});
 
-    const currentInactiveDevicesById = devices.inactive.reduce((acc, val) => {
-      acc[val.id] = val;
-      return acc;
-    }, {});
+    const currentInactiveDevicesById = this.state.devices.inactive.reduce(
+      (acc, val) => {
+        acc[val.id] = val;
+        return acc;
+      },
+      {}
+    );
 
-    const devicesById = props.browser.allDevices.reduce((acc, val) => {
+    const devicesById = this.props.browser.allDevices.reduce((acc, val) => {
       acc[val.id] = val;
       return acc;
     }, {});
 
     const inactiveDevices = [
-      ...props.browser.allDevices.filter(
+      ...this.props.browser.allDevices.filter(
         device =>
           !activeDevicesById[device.id] &&
           !currentInactiveDevicesById[device.id]
       ),
-      ...devices.inactive.filter(device => devicesById[device.id]),
+      ...this.state.devices.inactive.filter(device => devicesById[device.id]),
     ];
 
-    setDevices({active: activeDevices, inactive: inactiveDevices});
+    this.setState({
+      devices: {active: activeDevices, inactive: inactiveDevices},
+    });
 
     Mousetrap.bind('mod+f', () => {
       console.log('Working?');
       // inactiveDeviceList.current.setSearchOpen(true);
     });
-    return () => {
-      Mousetrap.unbind('mod+f');
-    };
-  }, [props.browser.devices, props.browser.allDevices]);
+  }
 
-  const onInactiveListFiltering = inactiveFiltered => {
-    setDevices({...devices, inactiveFiltered});
+  componentWillUnmount() {
+    Mousetrap.unbind('mod+f');
+  }
+
+  onInactiveListFiltering = f => {
+    this.setState((state, props) => ({
+      devices: {
+        active: state.devices.active,
+        inactive: state.devices.inactive,
+        inactiveFiltered: f,
+      },
+    }));
   };
 
-  const closeDialog = () => setOpen(false);
+  closeDialog = () => this.setState({open: false});
 
-  const onDragEnd = result => {
+  onDragEnd = result => {
     const {source, destination} = result;
 
     if (!source || !destination) {
       return;
     }
 
-    const sourceList = devices[source.droppableId];
-    const destinationList = devices[destination.droppableId];
+    const sourceList = this.state.devices[source.droppableId];
+    const destinationList = this.state.devices[destination.droppableId];
 
     if (!sourceList || !destinationList) {
       return;
@@ -99,94 +115,104 @@ export default function DeviceManager(props) {
 
     const itemDragged =
       source.droppableId === 'inactive'
-        ? devices.inactiveFiltered[source.index]
+        ? this.state.devices.inactiveFiltered[source.index]
         : sourceList[source.index];
 
     let idx = destination.index;
 
     if (destination.droppableId === 'inactive') {
       idx =
-        destination.index < devices.inactiveFiltered.length
-          ? devices.inactive.findIndex(
-              d => d.id === devices.inactiveFiltered[destination.index].id
+        destination.index < this.state.devices.inactiveFiltered.length
+          ? this.state.devices.inactive.findIndex(
+              d =>
+                d.id ===
+                this.state.devices.inactiveFiltered[destination.index].id
             )
-          : devices.inactive.length;
+          : this.state.devices.inactive.length;
     }
 
     sourceList.splice(sourceList.indexOf(itemDragged), 1);
 
     destinationList.splice(idx, 0, itemDragged);
 
-    updateDevices(devices);
+    this.updateDevices(this.state.devices);
   };
 
-  const updateDevices = devices => {
+  updateDevices = devices => {
     const active = [...devices.active];
     const inactive = [...devices.inactive];
-    setDevices({active, inactive});
-    props.setActiveDevices(active);
+    this.setState({devices: {active, inactive}});
+    this.props.setActiveDevices(active);
   };
 
-  return (
-    <Fragment>
-      <Button
-        variant="contained"
-        color="primary"
-        aria-label="upload picture"
-        component="span"
-        onClick={() => setOpen(true)}
-        className={styles.editButton}
-      >
-        Customize
-        {/* <EditIcon style={{fontSize: 'inherit'}} /> */}
-      </Button>
-      <Dialog fullScreen open={open}>
-        <AppBar className={classes.appBar} color="secondary">
-          <Toolbar>
-            {/* <IconButton edge="start" onClick={closeDialog} aria-label="close">
+  render() {
+    const {classes} = this.props;
+    return (
+      <Fragment>
+        <Button
+          variant="contained"
+          color="primary"
+          aria-label="upload picture"
+          component="span"
+          onClick={() => this.setState({open: true})}
+          className={styles.editButton}
+        >
+          Customize
+          {/* <EditIcon style={{fontSize: 'inherit'}} /> */}
+        </Button>
+        <Dialog fullScreen open={this.state.open}>
+          <AppBar className={classes.appBar} color="secondary">
+            <Toolbar>
+              {/* <IconButton edge="start" onClick={this.closeDialog} aria-label="close">
               <CloseIcon />
              </IconButton> */}
-            <Typography variant="h6" className={classes.title}>
-              Manage Devices
-            </Typography>
-            <Button color="inherit" onClick={closeDialog}>
-              close
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <div className={styles.container}>
-          <p className={styles.toolTip}>
-            <span>✨</span>Drag and drop the devices across to re-order them.
-          </p>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Grid container className={styles.content}>
-              <Grid item className={styles.section}>
-                <div className={styles.listTitle}>
-                  <LightBulbIcon height={30} color="#FFD517" />
-                  Active Devices
-                </div>
-                <DeviceList droppableId="active" devices={devices.active} />
+              <Typography variant="h6" className={classes.title}>
+                Manage Devices
+              </Typography>
+              <Button color="inherit" onClick={this.closeDialog}>
+                close
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <div className={styles.container}>
+            <p className={styles.toolTip}>
+              <span>✨</span>Drag and drop the devices across to re-order them.
+            </p>
+            <DragDropContext onDragEnd={this.onDragEnd}>
+              <Grid container className={styles.content}>
+                <Grid item className={styles.section}>
+                  <div className={styles.listTitle}>
+                    <LightBulbIcon height={30} color="#FFD517" />
+                    Active Devices
+                  </div>
+                  <DeviceList
+                    droppableId="active"
+                    devices={this.state.devices.active}
+                  />
+                </Grid>
+                <Grid item className={styles.section}>
+                  <div className={styles.listTitle}>
+                    <LightBulbIcon height={30} color="darkgrey" />
+                    Inactive Devices
+                  </div>
+                  <DeviceList
+                    droppableId="inactive"
+                    devices={this.state.devices.inactive}
+                    enableFiltering
+                    onFiltering={this.onInactiveListFiltering}
+                    enableCustomDeviceDeletion
+                    deleteDevice={this.props.deleteDevice}
+                    // ref={this.inactiveDeviceList}
+                  />
+                </Grid>
               </Grid>
-              <Grid item className={styles.section}>
-                <div className={styles.listTitle}>
-                  <LightBulbIcon height={30} color="darkgrey" />
-                  Inactive Devices
-                </div>
-                <DeviceList
-                  droppableId="inactive"
-                  devices={devices.inactive}
-                  enableFiltering
-                  onFiltering={onInactiveListFiltering}
-                  enableCustomDeviceDeletion
-                  deleteDevice={props.deleteDevice}
-                  // ref={inactiveDeviceList}
-                />
-              </Grid>
-            </Grid>
-          </DragDropContext>
-          <AddDeviceContainer />
-        </div>
-      </Dialog>
-    </Fragment>
-  );
+            </DragDropContext>
+            <AddDeviceContainer />
+          </div>
+        </Dialog>
+      </Fragment>
+    );
+  }
 }
+
+export default withStyles(additionalStyles)(DeviceManager);
