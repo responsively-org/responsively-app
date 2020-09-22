fc2ec4bc771a28e11a64bade4dc2536f0b1e2fd0
Dinesh Balaji Venkataraj <dvenkataraj@ztech.io>
2020-09-22 17:40:34 +0530

    refactor(desktop-app): remove routing dependencies
    remove react-router, react-router-dom, connected-react-router, history and related code
    
    Fixes: #414

--- desktop-app/app/store/configureStore.dev.js
+++ desktop-app/app/store/configureStore.dev.js
@@ -1,13 +1,9 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import {createHashHistory} from 'history';
-import {routerMiddleware, routerActions} from 'connected-react-router';
 import {createLogger} from 'redux-logger';
 import createRootReducer from '../reducers';
 
-const history = createHashHistory();
-
-const rootReducer = createRootReducer(history);
+const rootReducer = createRootReducer();
 
 const configureStore = initialState => {
   // Redux Configuration
@@ -28,14 +24,9 @@ const configureStore = initialState => {
     middleware.push(logger);
   }
 
-  // Router Middleware
-  const router = routerMiddleware(history);
-  middleware.push(router);
-
   // Redux DevTools Configuration
-  const actionCreators = {
-    ...routerActions,
-  };
+  const actionCreators = {};
+
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -64,4 +55,4 @@ const configureStore = initialState => {
   return store;
 };
 
-export default {configureStore, history};
+export default {configureStore};
