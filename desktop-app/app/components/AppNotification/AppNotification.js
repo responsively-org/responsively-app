c81f574cb79110405a10d7654741b49d25f8331f
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-10-14 14:18:53 +0530

    Fixed the typo

--- desktop-app/app/components/AppNotification/AppNotification.js
+++ desktop-app/app/components/AppNotification/AppNotification.js
@@ -41,7 +41,7 @@ const AppNotification = () => {
   const commonClasses = useCommonStyles();
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development1') {
+    if (process.env.NODE_ENV === 'development') {
       return;
     }
     (async () => {
