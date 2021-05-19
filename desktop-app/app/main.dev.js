e3bf976fae23f17b09b12a10f2244a31586e1c37
Manoj Vivek <p.manoj.vivek@gmail.com>
2021-05-19 23:50:24 +0530

    Restricted move to applications functionality to MacOS only

--- desktop-app/app/main.dev.js
+++ desktop-app/app/main.dev.js
@@ -257,7 +257,11 @@ app.on('ready', async () => {
   if (hasActiveWindow) {
     return;
   }
-  if (!app.isInApplicationsFolder() && (await confirmMove(dialog))) {
+  if (
+    process.platform === 'darwin' &&
+    !app.isInApplicationsFolder() &&
+    (await confirmMove(dialog))
+  ) {
     try {
       app.moveToApplicationsFolder({
         conflictHandler: conflictHandler.bind(this, dialog),
