9da831c61e0234e49cac05d15be6c2ca323a2091
durgakiran <kiranfet@gmail.com>
2023-04-15 17:47:47 +0530

    Add screenshot all functionality

--- desktop-app/src/renderer/components/ToolBar/index.tsx
+++ desktop-app/src/renderer/components/ToolBar/index.tsx
@@ -1,21 +1,74 @@
 import { useDispatch, useSelector } from 'react-redux';
 import {
+  selectIsCapturingScreenshot,
   selectIsInspecting,
   selectRotate,
+  setIsCapturingScreenshot,
   setIsInspecting,
   setRotate,
 } from 'renderer/store/features/renderer';
 import { Icon } from '@iconify/react';
+import { ScreenshotAllArgs } from 'main/screenshot';
+import { selectDevices } from 'renderer/store/features/device-manager';
+import WebPage from 'main/screenshot/webpage';
 import NavigationControls from './NavigationControls';
 import Menu from './Menu';
 import Button from '../Button';
 import AddressBar from './AddressBar';
 import ColorSchemeToggle from './ColorSchemeToggle';
+import ModalLoader from '../ModalLoader';
 
 const ToolBar = () => {
   const rotateDevice = useSelector(selectRotate);
   const isInspecting = useSelector(selectIsInspecting);
+  const isCapturingScreenshot = useSelector(selectIsCapturingScreenshot);
+  const devices = useSelector(selectDevices);
   const dispatch = useDispatch();
+
+  const screenshotCaptureHandler = async () => {
+    dispatch(setIsCapturingScreenshot(true));
+    const webViews: NodeListOf<Electron.WebviewTag> =
+      document.querySelectorAll('webView');
+    const screens: Array<ScreenshotAllArgs> = [];
+    webViews.forEach(async (webview) => {
+      const device = devices.find((d) => d.name === webview.id);
+      const webPage = new WebPage(webview as unknown as Electron.WebContents);
+      const pageHeight = await webPage.getPageHeight();
+      const previousHeight = webview.style.height;
+      const previousTransform = webview.style.transform;
+      webview.style.height = `${pageHeight}px`;
+      webview.style.transform = `scale(0.1)`;
+      if (device != null) {
+        screens.push({
+          webContentsId: webview.getWebContentsId(),
+          device,
+          previousHeight,
+          previousTransform,
+          pageHeight,
+        });
+      }
+    });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await window.electron.ipcRenderer.invoke<Array<ScreenshotAllArgs>, any>(
+      'screenshot:All',
+      screens
+    );
+
+    // reset webviews to original size
+    webViews.forEach((webview) => {
+      const screent = screens.find((s) => s.device.name === webview.id);
+      if (screent != null) {
+        webview.style.height = screent.previousHeight;
+        webview.style.transform = screent.previousTransform;
+      }
+    });
+
+    dispatch(setIsCapturingScreenshot(false));
+  };
+
+  const handleClose = () => {
+    // Do nothing. Prevent Dialog from closing.
+  };
 
   return (
     <div className="flex items-center justify-between gap-2">
@@ -41,8 +94,20 @@ const ToolBar = () => {
       >
         <Icon icon="lucide:inspect" />
       </Button>
+      <Button
+        onClick={screenshotCaptureHandler}
+        isActive={isCapturingScreenshot}
+        title="Screenshot All WebViews"
+      >
+        <Icon icon="lucide:camera" />
+      </Button>
       <ColorSchemeToggle />
       <Menu />
+      <ModalLoader
+        isOpen={isCapturingScreenshot}
+        onClose={handleClose}
+        title="loading..."
+      />
     </div>
   );
 };
