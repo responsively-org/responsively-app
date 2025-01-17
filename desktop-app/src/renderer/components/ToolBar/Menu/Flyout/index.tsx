00bfd2278f279bd226f5231fab8050399b94353a
Manoj Vivek <p.manoj.vivek@gmail.com>
2025-01-17 19:37:21 +0530

    Menu flyout layout tweaks

--- desktop-app/src/renderer/components/ToolBar/Menu/Flyout/index.tsx
+++ desktop-app/src/renderer/components/ToolBar/Menu/Flyout/index.tsx
@@ -1,4 +1,3 @@
-import MasonryLayout from 'renderer/components/Masonry/MasonryLayout';
 import Notifications from 'renderer/components/Notifications/Notifications';
 import { Divider } from 'renderer/components/Divider';
 import Devtools from './Devtools';
@@ -16,14 +15,16 @@ interface Props {
 
 const MenuFlyout = ({ closeFlyout }: Props) => {
   return (
-    <div className="absolute top-[26px] right-[4px] z-50 flex w-80 flex-col gap-2 rounded bg-white p-2 pb-0 text-sm shadow-lg ring-1 ring-slate-500 !ring-opacity-40 focus:outline-none dark:bg-slate-900 dark:ring-white dark:!ring-opacity-40">
+    <div className="absolute top-[26px] right-[4px] z-50 flex w-80 flex-col gap-2 rounded bg-slate-100 p-2 pb-0 text-sm shadow-lg ring-1 ring-slate-500 !ring-opacity-40 focus:outline-none dark:bg-slate-900 dark:ring-white dark:!ring-opacity-40">
       <Zoom />
-      <PreviewLayout />
-      <MasonryLayout />
       <UITheme />
       <Devtools />
       <AllowInSecureSSL />
       <ClearHistory />
+      <Divider />
+
+      <PreviewLayout />
+
       <Divider />
 
       <div>
