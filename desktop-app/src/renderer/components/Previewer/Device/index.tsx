c7da96df2b91b06717c7768adbd768876729bca0
sloemo01 <sloemodzn@gmail.com>
2026-07-25 06:32:03 +0530

    style(previewer): format minWidth expression for Prettier

--- desktop-app/src/renderer/components/Previewer/Device/index.tsx
+++ desktop-app/src/renderer/components/Previewer/Device/index.tsx
@@ -567,9 +567,7 @@ const Device = ({isPrimary, device, setIndividualDevice}: Props) => {
     <div
       className="h-fit"
       style={{
-        minWidth: isRestrictedMinimumDeviceSize
-          ? Math.max(scaledWidth, 208)
-          : undefined,
+        minWidth: isRestrictedMinimumDeviceSize ? Math.max(scaledWidth, 208) : undefined,
       }}
     >
       <div className="flex justify-between">
